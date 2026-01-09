79d282ec4fe7743dc6338444ae60f2886b7d34b1
PIYUSH THUL <thulpiyush6@gmail.com>
2026-01-10 02:05:32 +0530

    Implement singleNumber method to find unique number
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.
    
     
    
    Example 1:
    
    Input: nums = [2,7,11,15], target = 9
    Output: [0,1]
    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
    Example 2:
    
    Input: nums = [3,2,4], target = 6
    Output: [1,2]
    Example 3:
    
    Input: nums = [3,3], target = 6
    Output: [0,1]
     
    
    Constraints:
    
    2 <= nums.length <= 104
    -109 <= nums[i] <= 109
    -109 <= target <= 109
    Only one valid answer exists.
     
    
    Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?

--- twosum.java
+++ twosum.java
@@ -4,7 +4,7 @@ class Solution {
 
         for (int i = 0; i < nums.length; i++) {
             int rem = target - nums[i];
-            if (map.containsKey(rem)) {
+            if (map.containsKey(rem)) { 
                 return new int[]{map.get(rem), i};
             }
             map.put(nums[i], i);
